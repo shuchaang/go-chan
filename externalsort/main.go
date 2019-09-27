14f0fe106f6f0841cadc17d3eebd291db8c81c1c
shuchang <674137092@qq.com>
2019-09-27 17:43:56 +0800

    新增网络版

--- externalsort/main.go
+++ externalsort/main.go
@@ -5,31 +5,32 @@ import (
 	"fmt"
 	"go-chan/pipline"
 	"os"
+	"strconv"
 )
 
-func main(){
-	p:= createPipeline("large.in",100000000,100)
-	writeFile(p,"large.out")
+func main() {
+	p := createPipeline("large.in", 100000000, 100)
+	writeFile(p, "large.out")
 	printFile("large.out")
 }
 
 func printFile(filename string) {
 	file, e := os.Open(filename)
-	if e!=nil{
+	if e != nil {
 		panic(e)
 	}
 	defer file.Close()
 
 	source := pipline.ReaderSource(file, 10000)
 
-	for v:= range source{
+	for v := range source {
 		fmt.Println(v)
 	}
 }
 
 func writeFile(in <-chan int, filename string) {
 	file, e := os.Create(filename)
-	if e!=nil{
+	if e != nil {
 		panic(e)
 	}
 	defer file.Close()
@@ -37,21 +38,45 @@ func writeFile(in <-chan int, filename string) {
 	wr := bufio.NewWriter(file)
 	defer wr.Flush()
 
-	pipline.WriterSink(wr,in)
+	pipline.WriterSink(wr, in)
 }
 
-func createPipeline(filename string,fileSize,chunkCount int) <- chan int{
-	chunkSize:= fileSize/chunkCount
-	sortRes := [] <- chan int{}
-	for i:=0;i<chunkCount;i++{
-		file,e:=os.Open(filename)
-		if e!=nil{
+func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	sortRes := []<-chan int{}
+	for i := 0; i < chunkCount; i++ {
+		file, e := os.Open(filename)
+		if e != nil {
 			panic(e)
 		}
-		file.Seek(int64(i*chunkSize),0)
+		file.Seek(int64(i*chunkSize), 0)
 		source := pipline.ReaderSource(bufio.NewReader(file), chunkSize)
 		sort := pipline.InMemSort(source)
-		sortRes = append(sortRes,sort)
+		sortRes = append(sortRes, sort)
 	}
 	return pipline.MergeN(sortRes...)
+}
+
+func createNetPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	sortRes := []string{}
+	for i := 0; i < chunkCount; i++ {
+		file, e := os.Open(filename)
+		if e != nil {
+			panic(e)
+		}
+		file.Seek(int64(i*chunkSize), 0)
+		source := pipline.ReaderSource(bufio.NewReader(file), chunkSize)
+		sort := pipline.InMemSort(source)
+		addr := ":" + strconv.Itoa(7000+i)
+		pipline.NetWorkSink(addr, sort)
+		sortRes = append(sortRes, addr)
+	}
+	res := []<-chan int{}
+
+	for _, v := range sortRes {
+		source := pipline.NetWorkSource(v)
+		res = append(res, source)
+	}
+	return pipline.MergeN(res...)
 }
---
+++ pipline/net_nodes.go
@@ -0,0 +1,41 @@
+package pipline
+
+import (
+	"bufio"
+	"net"
+)
+
+func NetWorkSink(addr string, in <-chan int) {
+	listener, e := net.Listen("tcp", addr)
+	if e != nil {
+		panic(e)
+	}
+	go func() {
+		listener.Close()
+		conn, e := listener.Accept()
+		if e != nil {
+			panic(e)
+		}
+		defer conn.Close()
+		writer := bufio.NewWriter(conn)
+		WriterSink(writer, in)
+	}()
+}
+
+func NetWorkSource(addr string) <-chan int {
+	out := make(chan int)
+	go func() {
+		conn, e := net.Dial("tcp", addr)
+		if e != nil {
+			panic(e)
+		}
+		defer conn.Close()
+		source := ReaderSource(bufio.NewReader(conn), -1)
+
+		for v := range source {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
