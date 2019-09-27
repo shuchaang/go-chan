f3f1cf70239c4be87f802f0b1fa75f4a4de5afca
shuchang <674137092@qq.com>
2019-09-27 17:24:02 +0800

    add chan buffer

--- pipline/nodes.go
+++ pipline/nodes.go
@@ -7,47 +7,46 @@ import (
 	"sort"
 )
 
-func ArraySource(a ...int)chan  int{
+func ArraySource(a ...int) chan int {
 	out := make(chan int)
 	go func() {
-		for _,v := range a{
-			out<-v
+		for _, v := range a {
+			out <- v
 		}
 		close(out)
 	}()
 	return out
 }
 
-func InMemSort(in <- chan int)<- chan  int{
-	out:=make(chan  int)
+func InMemSort(in <-chan int) <-chan int {
+	out := make(chan int)
 	go func() {
-		a:=[]int{}
-		for v := range in{
-			a=append(a,v)
+		a := []int{}
+		for v := range in {
+			a = append(a, v)
 		}
 		sort.Ints(a)
 
-		for _,v:=range a{
-			out<-v
+		for _, v := range a {
+			out <- v
 		}
 		close(out)
 	}()
 	return out
 }
 
-
-func Merge(in1,in2 <-chan int)<- chan int{
-	out := make(chan  int)
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
 	go func() {
-		v1,ok1:= <-in1
-		v2,ok2:= <-in1
-		for ok1||ok2{
-			if !ok2 || (ok1&&v1<=v2){
-				out<-v1
-				v1,ok1=<-in1
-			}else{
-				out<-v2
-				v2,ok2=<-in2
+		v1, ok1 := <-in1
+		v2, ok2 := <-in1
+		for ok1 || ok2 {
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				v1, ok1 = <-in1
+			} else {
+				out <- v2
+				v2, ok2 = <-in2
 			}
 		}
 		close(out)
@@ -55,21 +54,19 @@ func Merge(in1,in2 <-chan int)<- chan int{
 	return out
 }
 
-
-
-func ReaderSource(reader io.Reader,chunkSize int)<- chan int{
-	out := make(chan  int)
+func ReaderSource(reader io.Reader, chunkSize int) <-chan int {
+	out := make(chan int, 1024)
 	go func() {
-		readSize:=0
-		buffer := make([]byte,8)
+		readSize := 0
+		buffer := make([]byte, 8)
 		for {
-			n,err := reader.Read(buffer)
-				readSize+=n
-			if n>0{
-				v:=int(binary.BigEndian.Uint64(buffer))
-				out<-v
+			n, err := reader.Read(buffer)
+			readSize += n
+			if n > 0 {
+				v := int(binary.BigEndian.Uint64(buffer))
+				out <- v
 			}
-			if err!=nil || (chunkSize!=-1&&chunkSize<readSize){
+			if err != nil || (chunkSize != -1 && chunkSize < readSize) {
 				break
 			}
 		}
@@ -78,32 +75,30 @@ func ReaderSource(reader io.Reader,chunkSize int)<- chan int{
 	return out
 }
 
-func WriterSink(writer io.Writer,in <- chan int)  {
-	for v:= range in{
-		buff := make([]byte,8)
-		binary.BigEndian.PutUint64(buff,uint64(v))
+func WriterSink(writer io.Writer, in <-chan int) {
+	for v := range in {
+		buff := make([]byte, 8)
+		binary.BigEndian.PutUint64(buff, uint64(v))
 		writer.Write(buff)
 	}
 }
 
-
-func RandomSource(count int)<-chan int{
-	out:=make(chan  int)
+func RandomSource(count int) <-chan int {
+	out := make(chan int)
 	go func() {
-		for i:=0;i<count;i++{
-			out<-rand.Int()
+		for i := 0; i < count; i++ {
+			out <- rand.Int()
 		}
 		close(out)
 	}()
 	return out
 }
 
-
-func MergeN(inputs ... <- chan int)<-chan int{
-	if len(inputs)==1{
+func MergeN(inputs ...<-chan int) <-chan int {
+	if len(inputs) == 1 {
 		return inputs[0]
 	}
-	m:=len(inputs)/2
+	m := len(inputs) / 2
 	// merge [0,m)  and [m,end)
-	return Merge(MergeN(inputs[:m]...),MergeN(inputs[m:]...))
+	return Merge(MergeN(inputs[:m]...), MergeN(inputs[m:]...))
 }
--- pipline/test/test.go
+++ pipline/test/test.go
@@ -7,35 +7,34 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 	file, e := os.Create("large.in")
-	if e!=nil{
+	if e != nil {
 		panic(e)
 	}
 	defer file.Close()
 	source := pipline.RandomSource(100000000)
-	f:=bufio.NewWriter(file)
-	pipline.WriterSink(f,source)
+	f := bufio.NewWriter(file)
+	pipline.WriterSink(f, source)
 	f.Flush()
-	file,e = os.Open("large.in")
-	if e!=nil{
+	file, e = os.Open("large.in")
+	if e != nil {
 		panic(e)
 	}
-	readerSource := pipline.ReaderSource(bufio.NewReader(file))
-	for v:=range readerSource{
-		fmt.Println(v)
-	}
+	//readerSource := pipline.ReaderSource(bufio.NewReader(file))
+	//for v:=range readerSource{
+	//	fmt.Println(v)
+	//}
 }
 
-
-func merge(){
+func merge() {
 	source := pipline.Merge(pipline.InMemSort(pipline.ArraySource(1, 4, 2, 65, 8, 4)),
 		pipline.InMemSort(pipline.ArraySource(1, 4, 2, 65, 8, 4)))
-	for{
+	for {
 		//close 之后都是0
-		if v,ok := <-source;ok{
+		if v, ok := <-source; ok {
 			fmt.Println(v)
-		}else{
+		} else {
 			break
 		}
 	}
